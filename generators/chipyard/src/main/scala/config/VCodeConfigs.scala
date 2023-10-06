ba877f4b182e171ba176779839c3dd71705346d7
Karl Hallsby <karl@hallsby.com>
2023-10-05 20:49:44 -0500

    Add slightly larger "Big" VCode Rocket configs

--- generators/chipyard/src/main/scala/config/VCodeConfigs.scala
+++ generators/chipyard/src/main/scala/config/VCodeConfigs.scala
@@ -14,9 +14,21 @@ class VCodeRocketConfig extends Config(
   new chipyard.config.AbstractConfig)
 // DOC include end: VCodeRocket
 
-/** This class is identical to the VCodeRocketConfig class, but includes
-  * synthesizing printf calls in the VCode RoCC accelerator for debugging.
-  */
+// DOC include start: BigVCodeRocket
+class BigVCodeRocketConfig extends Config(
+  new vcoderocc.WithVCodeAccel(8) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+// DOC include end: BigVCodeRocket
+
+/* These classes are identical to the regular VCodeRocketConfig classes, but
+ * include synthesizing printf calls in the VCode RoCC accelerator for debugging.
+ */
+
 class VCodeRocketPrintfConfig extends Config(
   new vcoderocc.WithVCodePrintf ++
   new VCodeRocketConfig)
+
+class BigVCodeRocketPrintfConfig extends Config(
+  new vcoderocc.WithVCodePrintf ++
+  new BigVCodeRocketConfig)
